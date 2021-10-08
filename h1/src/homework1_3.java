617f6d529e0ea25b9ac6b1f737701fd9d246992f
Wawayeah <1374498653@qq.com>
2021-10-08 16:17:55 +0800

    homework1_3

---
+++ h1/src/homework1_3.java
@@ -0,0 +1,14 @@
+
+public class homework1_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius=20;
+		double area;
+		area=radius*radius*3.14159;
+		System.out.println("The area for the circle of radius "+radius+" is "+area);
+		
+
+	}
+
+}
