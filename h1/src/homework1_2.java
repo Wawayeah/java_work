6ba8ba1a040d235774dff6a864d4e876feff248d
Wawayeah <1374498653@qq.com>
2021-10-08 15:58:41 +0800

    homework1_2

---
+++ h1/src/homework1_2.java
@@ -0,0 +1,11 @@
+
+public class homework1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("(10.5+2*3)/(45-3.5)= ");
+		System.out.println((10.5+2*3)/(45-3.5));
+
+	}
+
+}
