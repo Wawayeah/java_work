f762fe465ab64f0aadbf9a740c1587aca4545c22
Wawayeah <1374498653@qq.com>
2021-10-09 16:56:13 +0800

    homework1_5

---
+++ h1/src/homework1_5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class homework1_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("ÇëÊäÈëÊı×Ö");
+		double n1=input.nextDouble();
+		double n2=input.nextDouble();
+		double n3=input.nextDouble();
+		double a=(n1+n2+n3)/3;
+		System.out.print("a="+a);
+	}
+
+}
