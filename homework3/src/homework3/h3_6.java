1dbb08d027cb1091c18e40865ab6b680c92291a4
Wawayeah <1374498653@qq.com>
2021-10-16 21:06:11 +0800

    homework3_6

---
+++ homework3/src/homework3/h3_6.java
@@ -0,0 +1,20 @@
+package homework3;
+import java.util.*;
+public class h3_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a,b,distance;
+		Scanner input = new Scanner(System.in);
+		a=input.nextDouble();
+		b=input.nextDouble();
+		distance = Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
+		if(distance <= 10)
+			System.out.println("Point " + "(" + a + "," + b + ")"
+								+ " is in the circle");
+		else
+			System.out.println("Point " + "(" + a + "," + b + ")"
+								+ " is not in the circle");
+	}
+
+}
