cda137111680d701cdcf46a243df4a72b718b010
Wawayeah <1374498653@qq.com>
2021-10-30 15:05:25 +0800

    十进制->八进制

---
+++ homework3/src/homework3/h3_19.java
@@ -0,0 +1,12 @@
+package homework3;
+import java.util.*;
+public class h3_19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input= new Scanner(System.in);
+		int n = input.nextInt();
+		System.out.println(n+"'s OctalString is "+Integer.toOctalString(n));
+	}
+
+}
