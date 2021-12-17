bd365ee1b9d35618596cb964c222a768b0ff9db0
Wawayeah <1374498653@qq.com>
2021-12-17 16:33:41 +0800

    HashSet、Linked List、TreeSet

---
+++ homework9/src/homework9/LinkedListDemo.java
@@ -0,0 +1,36 @@
+package homework9;
+import java.util.*;
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+	   List<Cat> cats=new ArrayList<Cat>();
+	
+	   Cat tomcat=new Cat();
+	   tomcat.setName("Tom");
+	
+	   Cat jackcat=new Cat();
+	   jackcat.setName("Jack");
+	
+	   Cat johncat=new Cat();
+	   johncat.setName("John");
+	
+	   Cat anncat=new Cat();
+	   anncat.setName("Ann");	
+		
+		// 新建一个LinkedList
+	 LinkedList<Cat> stack=new LinkedList<Cat>();
+	 
+     // 元素压入栈中
+     stack.push(tomcat);
+     stack.push(jackcat);
+     stack.push(johncat);
+     stack.push(anncat);
+
+     // 删除栈顶元素
+     System.out.println("stack.pop():"+stack.pop().getName());
+
+     // 取出栈顶元素
+     System.out.println("stack.peek():"+stack.peek().getName());
+
+	}
+}
